3f3d0e8f315a9b3bc6c456a5f9c12ee453bf2c0b
palazkouruslan <r.palazkou@melsoft-games.com>
2024-02-20 07:28:56 +0300

    [feat]: improving

--- PumpGames/app/screens/tests/Tests.tsx
+++ PumpGames/app/screens/tests/Tests.tsx
@@ -1,16 +1,70 @@
+import GameCard from "@/components/ui/gameCard/GameCard"
+import Layout from "@/components/ui/layout/Layout"
 import TextComponent from "@/components/ui/text/TextComponent"
-import { View } from "react-native"
+import { AppConstants } from "@/constants/app.constants"
+import React from "react"
+import { ScrollView, View } from "react-native"
+import { useMediaQuery } from "react-responsive"
 
-const Tests = () => {
+const Tests = ({ navigation }: any) => {
+	const isDesktop = useMediaQuery({
+		query: "(min-width: 724px)"
+	})
 	return (
 		<View
 			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center"
+				flex: 1
 			}}
 		>
-			<TextComponent type='title'>Tests</TextComponent>
+			<ScrollView
+				style={{ flex: 1, backgroundColor: AppConstants.primaryBackground }}
+				contentContainerStyle={{
+					flexGrow: 1,
+					position: "relative",
+					justifyContent: "center",
+					borderWidth: 0,
+					alignItems: "center"
+				}}
+			>
+				<Layout navigation={navigation}>
+					<View
+						className='items-center w-full md:pb-[100px]'
+						style={{
+							height: "100%"
+						}}
+					>
+						<View className='mb-[20] w-full md:mb-[40px]'>
+							<TextComponent type='title'>Tests</TextComponent>
+						</View>
+						<View className='mb-[20] w-full md:mb-[80px]'>
+							<TextComponent>
+								Your
+								<TextComponent className='italic text-secondary opacity-80'>
+									{" "}
+									personal{" "}
+								</TextComponent>
+								selection of exercises{"\n"} for different brain areas
+							</TextComponent>
+						</View>
+						{/* GAMES */}
+						<View
+							className='md:flex-row md:justify-center md:items-start'
+							style={{ flex: 1 }}
+						>
+							<View className='items-center md:w-[300px]' style={{ flex: 1 }}>
+								<GameCard
+									source={require("@/assets/games/game_1.png")}
+									name='Math'
+									route='Math'
+									category='Problem solving'
+									navigation={navigation}
+								/>
+							</View>
+							{/*game 1*/}
+						</View>
+					</View>
+				</Layout>
+			</ScrollView>
 		</View>
 	)
 }
