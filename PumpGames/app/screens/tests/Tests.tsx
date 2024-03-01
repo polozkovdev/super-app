420ea381702a30f00f2aa26ec7fceb152c95f321
rpalazkou-ms <r.palazkou@moonactive.com>
2024-03-01 16:50:18 +0300

    [feat]: chess

--- PumpGames/app/screens/tests/Tests.tsx
+++ PumpGames/app/screens/tests/Tests.tsx
@@ -1,6 +1,8 @@
+import Layout from "@/components/ui/layout/Layout"
 import TextComponent from "@/components/ui/text/TextComponent"
+import { AppConstants } from "@/constants/app.constants"
 import React from "react"
-import { View } from "react-native"
+import { ScrollView, View } from "react-native"
 
 const Tests = ({ navigation }: any) => {
 	return (
@@ -9,7 +11,27 @@ const Tests = ({ navigation }: any) => {
 				flex: 1
 			}}
 		>
-			<TextComponent>Tests</TextComponent>
+			<ScrollView
+				style={{ flex: 1, backgroundColor: AppConstants.primaryBackground }}
+				contentContainerStyle={{
+					flexGrow: 1,
+					position: "relative",
+					justifyContent: "center",
+					borderWidth: 0,
+					alignItems: "center"
+				}}
+			>
+				<Layout navigation={navigation}>
+					<View
+						className='items-center w-full md:pb-[100px]'
+						style={{
+							height: "100%"
+						}}
+					>
+						<TextComponent>Tests</TextComponent>
+					</View>
+				</Layout>
+			</ScrollView>
 		</View>
 	)
 }
